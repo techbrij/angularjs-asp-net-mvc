779ed68a0bc63e31d9796e1e571e92f52a3562aa
techbrij <techbrij@gmail.com>
2013-09-18 23:30:02 +0530

    Base Version

---
+++ AngularMVC/Filters/MyValidateAntiForgeryTokenAttribute.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
+
+namespace AngularMVC.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class MyValidateAntiForgeryTokenAttribute : FilterAttribute, IAuthorizationFilter
+    {     
+       
+
+        private void ValidateRequestHeader(HttpRequestBase request)
+        {
+            string cookieToken = String.Empty;
+            string formToken = String.Empty;
+            string tokenValue = request.Headers["RequestVerificationToken"];           
+                if (!String.IsNullOrEmpty(tokenValue))
+                {
+                    string[] tokens = tokenValue.Split(':');
+                    if (tokens.Length == 2)
+                    {
+                        cookieToken = tokens[0].Trim();
+                        formToken = tokens[1].Trim();
+                    }
+                }
+            AntiForgery.Validate(cookieToken, formToken);
+        }
+
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+           
+            try
+            {
+                if (filterContext.HttpContext.Request.IsAjaxRequest())
+                {
+                    ValidateRequestHeader(filterContext.HttpContext.Request);
+                }
+                else
+                {
+                    AntiForgery.Validate();
+                }
+            }
+            catch (HttpAntiForgeryException e)
+            {
+                throw new HttpAntiForgeryException("Anti forgery token cookie not found");
+            }
+        }
+    }
+}
---
+++ AngularMVC/Scripts/SignUpController.js
@@ -0,0 +1,52 @@
+﻿//    Website: http://techbrij.com
+//    Program: AngularJS form validation
+
+var app = angular.module('validation', []);
+app.controller('SignUpController', function ($scope,$http) {
+    $scope.person = {};
+    $scope.sendForm = function () {
+        $http({
+            method: 'POST',
+            url: '/Account/SignUp',
+            data: $scope.person,
+            headers: {
+                'RequestVerificationToken':  $scope.antiForgeryToken 
+            }           
+        }).success(function (data, status, headers, config) {
+            $scope.message = '';
+            if (data.success == false) {
+                var str = '';
+                for (var error in data.errors) {
+                   str += data.errors[error] + '\n';
+                }
+                $scope.message = str;
+            }
+            else {
+                $scope.message = 'Saved Successfully';               
+                $scope.person = {};
+            }
+        }).error(function (data, status, headers, config) {                      
+            $scope.message = 'Unexpected Error';
+        });
+    };    
+});
+/* Directives */
+app.directive('ngUnique', ['$http', function (async) {
+    return {
+        require: 'ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+
+            elem.on('blur', function (evt) {
+                scope.$apply(function () {                   
+                    var val = elem.val();
+                    var req = { "userName": val }
+                    var ajaxConfiguration = { method: 'POST', url: '/Account/IsUserAvailable', data: req };
+                    async(ajaxConfiguration)
+                        .success(function(data, status, headers, config) {									
+                            ctrl.$setValidity('unique', data.result);
+                        });
+                });
+            });
+        }
+    }
+}]);
